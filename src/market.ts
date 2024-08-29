6e7ba081ea5de638a60a63535aef4f89ea61ae09
NanezX <victor.nanezj@gmail.com>
2024-08-28 21:08:17 -0400

    first commit

---
+++ src/market.ts
@@ -0,0 +1,12 @@
+import {
+  AggregatorAdded as AggregatorAddedEvent,
+  Buy as BuyEvent,
+  Claimed as ClaimedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+} from "../generated/SeabrickMarket/IMarket";
+import {
+  Approval,
+  ApprovalForAll,
+  OwnershipTransferred,
+  Transfer,
+} from "../generated/schema";
---
+++ src/seabrick.ts
@@ -0,0 +1,42 @@
+import {
+  Approval as ApprovalEvent,
+  ApprovalForAll as ApprovalForAllEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  Transfer as TransferEvent,
+} from "../generated/Seabrick/ISeabrick";
+import { OwnershipTransferred, Transfer } from "../generated/schema";
+
+export function handleApproval(event: ApprovalEvent): void {}
+
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.tokenId = event.params.tokenId;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
